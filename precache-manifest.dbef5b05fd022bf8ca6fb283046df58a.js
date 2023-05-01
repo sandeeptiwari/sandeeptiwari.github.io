cd7bc07125f8f2aac7ab4889a1a099af070d8186
Sandeep Tiwari <sandeep15mca@gmail.com>
2023-05-01 11:30:51 +0530

    Updates

---
+++ precache-manifest.dbef5b05fd022bf8ca6fb283046df58a.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47a55162747a3178fbe22bd1f56f7368",
+    "url": "/sandeep/index.html"
+  },
+  {
+    "revision": "0b639b878e98005f013e",
+    "url": "/sandeep/static/css/3.ce554f52.chunk.css"
+  },
+  {
+    "revision": "99ade0ff927d2747df0d",
+    "url": "/sandeep/static/css/4.c8ac464a.chunk.css"
+  },
+  {
+    "revision": "9cf2384e21bf747c518e",
+    "url": "/sandeep/static/css/main.e70c382e.chunk.css"
+  },
+  {
+    "revision": "76df9564adc74cc0c0aa",
+    "url": "/sandeep/static/js/2.cbb33305.chunk.js"
+  },
+  {
+    "revision": "0b639b878e98005f013e",
+    "url": "/sandeep/static/js/3.8cc14781.chunk.js"
+  },
+  {
+    "revision": "99ade0ff927d2747df0d",
+    "url": "/sandeep/static/js/4.53006dfa.chunk.js"
+  },
+  {
+    "revision": "9cf2384e21bf747c518e",
+    "url": "/sandeep/static/js/main.199a9b9d.chunk.js"
+  },
+  {
+    "revision": "c228aaad19c8f1bea634",
+    "url": "/sandeep/static/js/runtime-main.dd6d16f6.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/sandeep/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/sandeep/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/sandeep/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "a41f784840703e7c39e9d5ff37133c77",
+    "url": "/sandeep/static/media/developerActivity.a41f7848.svg"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "/sandeep/static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "b1648bd0d59662ef243e9e2c6cdf877b",
+    "url": "/sandeep/static/media/favicon.b1648bd0.ico"
+  },
+  {
+    "revision": "b1ae2adc77bafd0ab3f670d9ab06b5ca",
+    "url": "/sandeep/static/media/jsFramework.b1ae2adc.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/sandeep/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "e62302433c1e778b06dd2e983e9306c5",
+    "url": "/sandeep/static/media/lti.e6230243.png"
+  },
+  {
+    "revision": "a15c230b794fda4eb28f2320529072a1",
+    "url": "/sandeep/static/media/manOnTable.a15c230b.svg"
+  },
+  {
+    "revision": "1fbec64fbc806b563ac7c985f31e1df8",
+    "url": "/sandeep/static/media/meta_pic.1fbec64f.jpg"
+  },
+  {
+    "revision": "f041d99793f0c15a757a3d8b73d937a8",
+    "url": "/sandeep/static/media/pierian-data-logo.f041d997.png"
+  },
+  {
+    "revision": "4b2938b9f5ea2f1ddbd45dccaff628bc",
+    "url": "/sandeep/static/media/quoraLogo.4b2938b9.png"
+  },
+  {
+    "revision": "05b89315da86d827606f827e50c8e7ca",
+    "url": "/sandeep/static/media/rahulshettyacademy-logo.05b89315.png"
+  },
+  {
+    "revision": "1591c202ea86be03349f8d565ac1f959",
+    "url": "/sandeep/static/media/seetest-logo.1591c202.png"
+  },
+  {
+    "revision": "96c9d7a913fc06de9bd7e13f5cb26af9",
+    "url": "/sandeep/static/media/skill.96c9d7a9.svg"
+  },
+  {
+    "revision": "7ebc764bf5f86dd0e65964bc4a734411",
+    "url": "/sandeep/static/media/vskills-logo.7ebc764b.png"
+  },
+  {
+    "revision": "d0a70af7f35f5943c7e28883adbbb7fd",
+    "url": "/sandeep/static/media/zero-to-mastery.d0a70af7.png"
+  }
+]);
